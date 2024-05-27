b13caa37003cf94b0a5573c7b16580397c12176e
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-05-27 17:58:14 +0300

    completed assignments

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
+    def __init__(self,first_name,last_name):
+        super().__init__(first_name,last_name)
+        self.knowledge = []
     
-    def learn(self):
+    def learn(self,string):
+        self.knowledge.append (string)
         pass
